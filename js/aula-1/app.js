b242feeff7418aca2c8858ff773ebf9331b0f4a1
ogustavxs <gustavopessoasillva@gmail.com>
2025-04-03 22:45:06 -0300

    info: matematica e concatenação

--- js/aula-1/app.js
+++ js/aula-1/app.js
@@ -15,3 +15,22 @@ console.log(idade)
 let resultado = confirm("Você tem certeza que deseja continuar?")
 
 console.log(`Ola ${nome}, sua idade é ${idade} e o resultado da confirmação é ${resultado}`)
+
+/*
++ (Soma)
+- (Subtração)
+* (Multiplicação)
+/ (Divisão)
+% (Resto)
+** (Potencia)
+*/
+
+let numero1 = prompt("Digite o primeiro número")
+let numero2 = prompt("Digite o segundo número")
+
+let result = numero1 + numero2
+
+console.log(result)
+
+//parseFloat
+//parseInt
