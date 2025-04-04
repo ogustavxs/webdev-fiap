07beae0cbb397d490b9de67a0f84bb5637e39c35
ogustavxs <gustavopessoasillva@gmail.com>
2025-04-03 22:27:47 -0300

    info: comandos basicos do JavaScript

---
+++ js/aula-1/app.js
@@ -0,0 +1,17 @@
+// alert("Hello World!");
+console.log("Hello Again!")
+
+// Variaveis
+let nome
+const sexo = "M"
+
+nome = "Gustavo"
+console.log(nome)
+console.log(typeof sexo)
+
+let idade = prompt("Qual a sua idade?", "Insira aqui sua idade")
+console.log(idade)
+
+let resultado = confirm("Você tem certeza que deseja continuar?")
+
+console.log(`Ola ${nome}, sua idade é ${idade} e o resultado da confirmação é ${resultado}`)
