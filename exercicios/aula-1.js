0fdcb16821a7b3ce3e2e1a48ddec22205a2b96ff
ogustavxs <gustavopessoasillva@gmail.com>
2025-04-03 22:44:28 -0300

    task: exercicio da aula 1

---
+++ exercicios/aula-1.js
@@ -0,0 +1,6 @@
+let nome = prompt("Qual o seu nome?")
+let idade = prompt("Qual a sua idade?")
+let profissao = prompt("Qual a sua profissão?")
+
+
+console.log(`Olá ${nome}, voce tem ${idade} anos e trabalha como ${profissao}!`)
