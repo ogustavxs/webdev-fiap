fe31188ddb176063941f594eefe63c985907b42e
ogustavxs <gustavopessoasillva@gmail.com>
2025-04-10 22:06:27 -0300

    info: Condicionais

---
+++ js/aula-2/app.js
@@ -0,0 +1,30 @@
+console.log(5=="5");
+console.log(5==="5");
+console.log('0'==false)
+console.log('0'===false);
+console.log(''==0)
+console.log(''===0)
+console.log("uva">"banana")
+
+
+let nome = "gustavo"
+
+if (nome === "gustavo") {
+    console.log("O nome é gustavo")
+}
+else if (nome === "gabriel") {
+    console.log("O nome é gabriel")
+}
+else {
+    console.log("Definitivamente não são gemeos")
+}
+
+nome === "gustavo" ? console.log("O nome é gustavo") : console.log("O nome não é gustavo")
+
+if (nome === "gusavo" || nome == null) {
+    console.log("as coisas deram certo")
+}
+
+if (nome === "gustavo" && nome != null) {
+    console.log("as coisas deram certo")
+}
