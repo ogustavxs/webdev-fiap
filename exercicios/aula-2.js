1d46092c9389a781ef23ed870f9144d88126bbcb
ogustavxs <gustavopessoasillva@gmail.com>
2025-04-10 22:24:39 -0300

    feat: primeiro exercicio completo

---
+++ exercicios/aula-2.js
@@ -0,0 +1,10 @@
+login = prompt("Digite seu login: ");
+senha = prompt("Digite sua senha: ");
+
+if (login == "1234" && senha == "1234") {
+    alert("Login efetuado com sucesso!");
+}
+else {
+    alert("login ou senha incorretos!");
+}
+
